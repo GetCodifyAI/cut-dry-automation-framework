03d35ba262b04093264b2d5b5983f6c568a77375
senuri-mr <senuri@cutanddry.com>
2024-11-12 13:03:51 +0530

    Added new tests

---
+++ src/test/java/com/cutanddry/qa/tests/track/VerifyTheTrackNotificationsPushTest.java
@@ -0,0 +1,40 @@
+package com.cutanddry.qa.tests.track;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Track;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyTheTrackNotificationsPushTest extends TestBase {
+    static User user;
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-535")
+    public void VerifyTheTrackNotificationsPush() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToTrackNotifications();
+        softAssert.assertTrue(Track.isNotificationsTextDisplayed(),"navigation to track notif error");
+
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
