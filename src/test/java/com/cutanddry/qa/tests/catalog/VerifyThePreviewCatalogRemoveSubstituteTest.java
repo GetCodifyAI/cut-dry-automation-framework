86210d516e06715effbe054cbda3db1d33489155
senuri-mr <senuri@cutanddry.com>
2024-11-21 15:12:23 +0530

    Added

---
+++ src/test/java/com/cutanddry/qa/tests/catalog/VerifyThePreviewCatalogRemoveSubstituteTest.java
@@ -0,0 +1,50 @@
+package com.cutanddry.qa.tests.catalog;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Catalog;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyThePreviewCatalogRemoveSubstituteTest extends TestBase {
+    static User user;
+    String substituteItemCode = "20024";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-616")
+    public void VerifyThePreviewCatalogRemoveSubstitute() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToCatalog();
+        softAssert.assertTrue(Catalog.isUserNavigatedToCatalog(),"navigation error");
+        Catalog.clickOnPreviewCatalog();
+        softAssert.assertTrue(Catalog.isNavigatedToPreviewCatalog(),"navigation to preview catalog error");
+        Catalog.selectFirstItem();
+        softAssert.assertTrue(Catalog.isProductOverviewDisplayed(),"select product error");
+        Catalog.clickOnEditProduct();
+        Catalog.navigateToSubstituteTab();
+        Catalog.deleteSubstituteItem(substituteItemCode);
+        Catalog.saveChanges();
+        softAssert.assertTrue(Catalog.successOverlayDisplayed(),"Error in Removing substitute item");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
