dfed54d5be9bd8ee462ca8438db38e48939d4ba7
virangabandara <viranga@cutanddry.com>
2024-11-25 14:07:09 +0530

    Add VerifyTheTrackerIssuesTest

--- src/main/java/com/cutanddry/qa/functions/Tracker.java
+++ src/main/java/com/cutanddry/qa/functions/Tracker.java
@@ -8,4 +8,14 @@ public class Tracker {
     public static boolean isOnboardingDocsDisplayed() throws InterruptedException{
         return trackerPage.isOnboardingDocsDisplayed();
     }
+
+    public static void clickIssueTracker(){trackerPage.clickIssueTracker();}
+    public static boolean isIssueTrackerDisplayed() throws InterruptedException{
+        return trackerPage.isIssueTrackerDisplayed();
+    }
+    public static void clickNewTicket(){trackerPage.clickNewTicket();}
+    public static boolean isCreateNewIssueDisplayed() throws InterruptedException{
+        return trackerPage.isCreateNewIssueDisplayed();
+    }
+
 }
--- src/main/java/com/cutanddry/qa/pages/TrackerPage.java
+++ src/main/java/com/cutanddry/qa/pages/TrackerPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 public class TrackerPage extends LoginPage{
 
     By txt_onboarding = By.xpath("//div[@class='_1ducyq8 mont _1npprf5' and text()='Onboarding Docs']");
+    By btn_issue_tracker = By.xpath("//a[@class='_zzikwb4 text-center nav-item nav-link' and text()='Issue Tracker']");
+    By txt_task_tracker = By.xpath("//div[@class='_1ducyq8 mont mb-2 _xdqbqk6' and text()='Task Tracker']");
+    By btn_new_ticket = By.xpath("//button[@class='d-flex align-items-center justify-content-center cdbutton _1c2qb1w btn btn-primary btn-sm' and text()='New Ticket']");
+    By txt_create_new_issue = By.xpath("//div[@class='p-0 mx-4 mt-4 pb-4 modal-header']//span[@class='_1tcxgp3 _wfze9d' and text()='Create New Issue']");
 
 
     public boolean isOnboardingDocsDisplayed(){
@@ -13,6 +17,28 @@ public class TrackerPage extends LoginPage{
             return false;
         }
         return distributorUI.isDisplayed(txt_onboarding);
+    }
+    public void clickIssueTracker(){
+        distributorUI.click(btn_issue_tracker);
+    }
+    public boolean isIssueTrackerDisplayed(){
+        try {
+            distributorUI.waitForVisibility(txt_task_tracker);
+        } catch (Exception e){
+            return false;
+        }
+        return distributorUI.isDisplayed(txt_task_tracker);
+    }
+    public void clickNewTicket(){
+        distributorUI.click(btn_new_ticket);
+    }
+    public boolean isCreateNewIssueDisplayed(){
+        try {
+            distributorUI.waitForVisibility(txt_create_new_issue);
+        } catch (Exception e){
+            return false;
+        }
+        return distributorUI.isDisplayed(txt_create_new_issue);
     }
 
 }
