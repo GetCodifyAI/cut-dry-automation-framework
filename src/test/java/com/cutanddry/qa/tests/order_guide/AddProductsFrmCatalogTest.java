1b90bf5f4b0574a106c1606d60b38984e89fd3b4
Ahsan Mubarak <ahsan@Mohameds-MacBook-Pro.local>
2024-12-18 00:32:54 +0530

    Bug fix for stabilizing the regression suite in Distributor Part One

--- src/test/java/com/cutanddry/qa/tests/order_guide/AddProductsFrmCatalogTest.java
+++ src/test/java/com/cutanddry/qa/tests/order_guide/AddProductsFrmCatalogTest.java
@@ -16,7 +16,9 @@ import org.testng.asserts.SoftAssert;
 public class AddProductsFrmCatalogTest extends TestBase {
     static User user;
     static String customerId = "16579";
-    static String itemName = "Artichoke";
+//    static String itemName = "Artichoke";
+    static String itemName, searchItemCode;
+    static double itemPrice;
 
 
     @BeforeMethod
@@ -31,16 +33,27 @@ public class AddProductsFrmCatalogTest extends TestBase {
         Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
         Dashboard.isUserNavigatedToDashboard();
         softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+
         Dashboard.navigateToCustomers();
         Customer.searchCustomerByCode(customerId);
         softAssert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(customerId),"search error");
         Customer.clickOnOrderGuide(customerId);
+
+        itemName = Customer.getItemNameFirstRow();
+        searchItemCode = Customer.getItemCodeFirstRow();
+        itemPrice = Customer.getActiveItemPriceFirstRow();
         Customer.goToCatalog();
-        Customer.searchItemOnCatalog(itemName);
+
+//        Customer.searchItemOnCatalog(itemName);
+//        softAssert.assertTrue(Customer.getFirstElementFrmSearchResults(itemName).contains(itemName), "item not found");
+//        Customer.addItemToCartCatalog(itemName);
+//        Customer.checkoutItems();
+//        softAssert.assertTrue(Customer.getItemNameFirstRow().contains(itemName),"item mismatch");
+        Customer.searchItemOnCatalog(searchItemCode);
         softAssert.assertTrue(Customer.getFirstElementFrmSearchResults(itemName).contains(itemName), "item not found");
         Customer.addItemToCartCatalog(itemName);
+        softAssert.assertEquals(Customer.getItemPriceOnCheckoutButton(),itemPrice,"The item has not been selected.");
         Customer.checkoutItems();
-        softAssert.assertTrue(Customer.getItemNameFirstRow().contains(itemName),"item mismatch");
         softAssert.assertAll();
     }
 
