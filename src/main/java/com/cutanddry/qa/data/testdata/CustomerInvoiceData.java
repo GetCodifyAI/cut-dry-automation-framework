860c54849ec4d7a4ab9350c315c0cf00d46ef0d7
asheneo <subodha162@gmail.com>
2024-12-21 01:15:26 +0530

    new data

--- src/main/java/com/cutanddry/qa/data/testdata/CustomerInvoiceData.java
+++ src/main/java/com/cutanddry/qa/data/testdata/CustomerInvoiceData.java
@@ -14,4 +14,9 @@ public class CustomerInvoiceData
         public static final String BOOK_KEEPER_EMAIL = "test123@gmail.com";
         public static final String BOOK_KEEPER_MOBILE = "0712222222";
         public static final String NOTIFICATION_EMAIL = "test123@gmail.com";
+        public static final String CREDIT_MEMO_NUMBER = "CM51286750";
+        public static final String ASSOCIATED_INVOICE = "51286300 - $351.00";
+        public static final String CREDIT_MEMO_AMOUNT = "100";
+        public static final String CREDIT_MEMO_DESCRIPTION = "Test Description";
+
     }
