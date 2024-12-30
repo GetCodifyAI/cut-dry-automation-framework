2ceac280ebc3b789ae043fe524b4f4ab4bef99db
virangabandara <viranga@cutanddry.com>
2024-12-30 23:57:05 +0530

    add VerifyOrderSettingEditOrderReminderAlertsTest

---
+++ src/test/java/com/cutanddry/qa/tests/settings/VerifyOrderSettingEditOrderReminderAlertsTest.java
@@ -0,0 +1,53 @@
+package com.cutanddry.qa.tests.settings;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Settings;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.net.URISyntaxException;
+
+public class VerifyOrderSettingEditOrderReminderAlertsTest extends TestBase {
+    static User user;
+    String DistributorName ="96939129 - Cut+Dry Agent - Hillcrest Foodservice";
+    String Option = "3 hour before cutoff time";
+
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-861")
+    public void VerifyOrderSettingAddOrderReminderAlerts() throws InterruptedException, URISyntaxException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurant(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToDistributorPortal(DistributorName);
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"navigation error");
+        Dashboard.navigateToOrderSettings();
+        softAssert.assertTrue(Settings.isOrderSettingsTextDisplayed(),"navigation to order settings error");
+        Settings.clickAddAlert();
+        softAssert.assertTrue(Settings.isOrderRemindAlertTextDisplayed(),"Order remind alert pop up not display");
+        Settings.clickSendAlertDropDown();
+        Settings.clickSendAlertOption(Option);
+        Settings.clickOnSave();
+        softAssert.assertTrue(Settings.isSettingOrderReminderTextDisplayed(),"Settings yor order reminder pop up not displayed");
+        Settings.clickOkAlert();
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
