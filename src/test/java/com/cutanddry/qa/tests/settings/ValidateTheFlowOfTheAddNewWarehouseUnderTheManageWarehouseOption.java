06643c7586541f64add61eef2941255084682638
asheneo <subodha162@gmail.com>
2025-01-01 01:31:20 +0530

    ValidateTheFlowOfTheAddNewWarehouseUnderTheManageWarehouseOption

---
+++ src/test/java/com/cutanddry/qa/tests/settings/ValidateTheFlowOfTheAddNewWarehouseUnderTheManageWarehouseOption.java
@@ -0,0 +1,45 @@
+package com.cutanddry.qa.tests.settings;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Settings;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class ValidateTheFlowOfTheAddNewWarehouseUnderTheManageWarehouseOption extends TestBase {
+    static User user;
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-852")
+    public void ValidateTheFlowOfTheAddNewWarehouseUnderTheManageWarehouseOption() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToTrackSettings();
+        softAssert.assertTrue(Settings.isTrackSettingsTextDisplayed(),"navigation error");
+        Settings.clickBtnManageWarehouse();
+        Settings.clickBtnAddNew();
+        String warehouseName = Settings.generateWarehouseCode();
+        Settings.typeNewWarehouseName(warehouseName);
+        Settings.clickBtnSaveChanges();
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
