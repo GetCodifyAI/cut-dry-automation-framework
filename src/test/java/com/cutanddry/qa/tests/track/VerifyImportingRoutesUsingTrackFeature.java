07f380dd3a7e77012fa509d5d313a868ad18426e
asheneo <subodha162@gmail.com>
2025-01-06 09:56:09 +0530

    Save changes before merging main

---
+++ src/test/java/com/cutanddry/qa/tests/track/VerifyImportingRoutesUsingTrackFeature.java
@@ -0,0 +1,43 @@
+package com.cutanddry.qa.tests.track;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Track;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyImportingRoutesUsingTrackFeature extends TestBase{
+    static User user;
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-534")
+    public void VerifyTheTrackNotificationsEmail() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+//        Dashboard.navigateToTrackNotifications();
+//        softAssert.assertTrue(Track.isNotificationsTextDisplayed(),"navigation to track notif error");
+//        Track.clickOnNotifExample();
+//        softAssert.assertTrue(Track.isNotificationExamplePopupDisplayed(),"notif example popup error");
+//        Track.clickOnEmail();
+//        softAssert.assertTrue(Track.isEmailTemplateAndPreviewDisplayed(),"email notif error");
+        softAssert.assertAll();
+    }
+
+//    @AfterMethod
+//    public void tearDown(ITestResult result) {
+//        takeScreenshotOnFailure(result);
+//        closeAllBrowsers();
+//    }
+}
