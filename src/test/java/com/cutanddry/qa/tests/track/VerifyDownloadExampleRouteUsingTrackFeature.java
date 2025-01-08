758a01a886bb4e690c8dcad906321b12de95d480
asheneo <subodha162@gmail.com>
2025-01-08 14:26:28 +0530

    VerifyDownloadExampleRouteUsingTrackFeature

---
+++ src/test/java/com/cutanddry/qa/tests/track/VerifyDownloadExampleRouteUsingTrackFeature.java
@@ -0,0 +1,45 @@
+package com.cutanddry.qa.tests.track;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.data.testdata.TrackData;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Track;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyDownloadExampleRouteUsingTrackFeature extends TestBase {
+    static User user;
+    static String distributorName = "Brandon IFC Cut+Dry Agent";
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-867")
+    public void VerifyDownloadExampleRouteUsingTrackFeature() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurant(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToLoginAsPortal(distributorName);
+        Dashboard.navigateToTrackRoutes();
+        Track.clickOkIfErrorTextDisplayed();
+        Track.deleteExistingRoute();
+        Track.clickBtnManageRoutes();
+        Track.clickBtnDownloadExampleFile();
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
