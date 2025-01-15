82d07b08014154151d005090d47b9d3606ae8f9e
ashaneo <subodha162@gmail.com>
2025-01-15 11:09:04 +0530

    VerifyDateChangeFunctionalityUnderMonitoring

---
+++ src/test/java/com/cutanddry/qa/tests/track/VerifyDateChangeFunctionalityUnderMonitoring.java
@@ -0,0 +1,49 @@
+package com.cutanddry.qa.tests.track;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Pay;
+import com.cutanddry.qa.functions.Track;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.net.URISyntaxException;
+
+public class VerifyDateChangeFunctionalityUnderMonitoring extends TestBase {
+    static User user;
+    static String distributorName = "Brandon IFC Cut+Dry Agent";
+    String startDay = "Sunday";
+    String startMonth = "December";
+    String startDate = "1";
+    String startYear = "2024";
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-873")
+    public void VerifyDateChangeFunctionalityUnderMonitoring() throws InterruptedException, URISyntaxException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurant(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToLoginAsPortal(distributorName);
+        Dashboard.navigateToTrackMonitoring();
+        softAssert.assertTrue(Track.isMonitoringTextDisplayed(),"navigation to track monitoring error");
+        Track.clickOkIfErrorTextDisplayed();
+        Track.selectDateMonitoring(startDay,  startMonth,  startDate,  startYear);
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
