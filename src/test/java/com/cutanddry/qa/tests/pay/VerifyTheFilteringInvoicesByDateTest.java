4131fbb6535f6ea4a7a61d0797897c64bf82dee6
virangabandara <viranga@cutanddry.com>
2025-01-21 11:29:12 +0530

    add change

--- src/test/java/com/cutanddry/qa/tests/pay/VerifyTheFilteringInvoicesByDateTest.java
+++ src/test/java/com/cutanddry/qa/tests/pay/VerifyTheFilteringInvoicesByDateTest.java
@@ -47,9 +47,9 @@ public class VerifyTheFilteringInvoicesByDateTest extends TestBase {
         softAssert.assertAll();
     }
 
-//    @AfterMethod
-//    public void tearDown(ITestResult result) {
-//        takeScreenshotOnFailure(result);
-//        closeAllBrowsers();
-//    }
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
 }
