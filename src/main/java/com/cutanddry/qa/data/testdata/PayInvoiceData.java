6ebf2cde62130586e3bfd51479aa35097c233961
AhsanMubarak <ahsan@cutanddry.com>
2025-02-19 17:33:04 +0530

    stabilizing the regression suite in Distributor

--- src/main/java/com/cutanddry/qa/data/testdata/PayInvoiceData.java
+++ src/main/java/com/cutanddry/qa/data/testdata/PayInvoiceData.java
@@ -4,7 +4,7 @@ public class PayInvoiceData
     {
         public static final String CUSTOMER_NAME = "Bellota Restaurant - Sacramento ";
 
-        public static final String STATUS_PAST_DUE = "Past due";
+        public static final String STATUS_PAST_DUE = "- Past due";
         public static final String STATUS_UNPAID = "Unpaid";
         public static final String STATUS_SCHEDULED = "Scheduled";
         public static final String STATUS_PROCESSING = "Processing";
