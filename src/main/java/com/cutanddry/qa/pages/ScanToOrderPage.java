c81c9a57e39f6ebfe983820bc31d1f486a9e8ce4
wishminipalihawadana <wishmini@cutanddry.com>
2025-03-11 08:48:47 +0530

    Automating Scan to Order test cases DOT-TC-1117 DOT-TC-1121 DOT-TC-1115 DOT-TC-1114

--- src/main/java/com/cutanddry/qa/pages/ScanToOrderPage.java
+++ src/main/java/com/cutanddry/qa/pages/ScanToOrderPage.java
@@ -14,6 +14,19 @@ public class ScanToOrderPage extends LoginPage{
     String quantityDecreaseMinusBtn = "//div[contains(@class,'cartContainer')]//span[contains(text(),'ITEMCODE')]/../following-sibling::div//*[name()='svg' and @data-icon='minus']";
     String itemPrice = "//div[contains(@class,'cartContainer')]//span[contains(text(),'ITEMCODE')]/../following-sibling::div/div[1]";
 
+    By totalQuantityText = By.xpath("//span[contains(text(),'Total Quantity')]");
+    By totalLineItemsText = By.xpath("//span[contains(text(),'Total Line Items')]");
+    String totalQuantityOrderSummary = "//span[contains(text(),'Total Quantity')]/following-sibling::*";
+    By cartContainerItems = By.xpath("//div[@class= 'cartItem_18e3qyo']");
+    String totalLineItemsOrderSummary = "//span[contains(text(),'Total Line Items')]/following-sibling::*";
+    By totalEstimatedCostText = By.xpath("//div[contains(text(),'Total Estimated Cost')]");
+    String totalPriceOrderSummary = "//div[contains(text(),'Total Estimated Cost')]/following-sibling::*";
+    By totalDiscountsText = By.xpath("//span[contains(text(),'Total Discounts')]");
+
+    By searchInputField = By.xpath("//input[@placeholder='Scan barcode or search item by name or code']");
+    String emptyCartText_1 = "//h3[text()='VALUE']";
+    String emptyCartText_2 = "//p[@class='productMeta_fxekb7']";
+
 
     public boolean isScanToOrderTextDisplayed(){
         return distributorUI.isDisplayed(ScanToOrderText);
@@ -96,6 +109,100 @@ public class ScanToOrderPage extends LoginPage{
         }
         distributorUI.click(ReviewAndConfirmBtn);
     }
+    public boolean isTotalQuantityTextDisplayed()
+    {
+        return distributorUI.isDisplayed(totalQuantityText);
+    }
+
+    public boolean isTotalEstimatedCostTextDisplayed()
+    {
+        return distributorUI.isDisplayed(totalEstimatedCostText);
+    }
+
+
+    public boolean isTotalLineItemsTextDisplayed()
+    {
+        return distributorUI.isDisplayed(totalLineItemsText);
+    }
+
+    public boolean isTotalDiscountsTextDisplayed(){
+        return distributorUI.isDisplayed(totalDiscountsText);
+    }
+
+    public double getTotalCostInOrderSummary()
+    {
+        try {
+            distributorUI.waitForCustom(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String totalPriceInString = distributorUI.getText(By.xpath(totalPriceOrderSummary));
+        return Double.parseDouble(totalPriceInString.replace("$", ""));
+    }
+    public int getCartLineItemCount(){
+        int ItemCount = distributorUI.countElements(cartContainerItems);
+        return ItemCount;
+    }
+
+    public int getProductQuantityValues(String xpath) {
+        String  quantityValue =  distributorUI.getText(By.xpath(xpath), "value");
+        return Integer.parseInt(quantityValue);
+    }
+
+    public int getProductQuantityValue(){
+        int totalProducts = getProducttotalQuantityValues();
+        int TotalProductQuantity = 0;
+
+        for(int i = 1; i <= totalProducts; i++){
+            String x = String.format("(//div[@class='cartItem_18e3qyo'])[%d]//descendant::input[@data-input='quantityInput']",i);
+            TotalProductQuantity = TotalProductQuantity+ getProductQuantityValues(x);
+        }
+        return TotalProductQuantity;
+    }
+
+    public int getProducttotalQuantityValues() {
+        String quantityValue2 = distributorUI.getText(By.xpath(totalQuantityOrderSummary));
+        String quantityOnly = quantityValue2.replaceAll("\\D", "");
+
+        return Integer.parseInt(quantityOnly);
+    }
+
+    public int getCartItemCountInOrderSummary(){
+        String itemCountInOrderSummary = distributorUI.getText(By.xpath(totalLineItemsOrderSummary));
+        String itemCountOnly = itemCountInOrderSummary.replaceAll("\\D","");
+
+        return Integer.parseInt(itemCountOnly);
+    }
+
+    public boolean isSearchInputFieldTextDisplayed(){
+        return distributorUI.isDisplayed(searchInputField);
+    }
+
+    public String getSearchInputFieldText(){
+        return distributorUI.getText(searchInputField,"placeholder");
+    }
+
+    public String getCTAButtonText(){
+        String CTAbuttonText = distributorUI.getText(ReviewAndConfirmBtn);
+        return CTAbuttonText;
+    }
+
+    public boolean isEmptyCartTextDisplayed(String value){
+        return distributorUI.isDisplayed(By.xpath(emptyCartText_1.replace("VALUE", value)));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
