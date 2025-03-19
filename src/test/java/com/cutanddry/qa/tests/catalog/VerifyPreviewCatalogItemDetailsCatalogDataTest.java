43d067b6adf67cbec3a93bc3e9a32e585dcdb119
AhsanMubarak <ahsan@cutanddry.com>
2025-03-19 10:58:54 +0530

    Stabilizing the regression suite in Distributor

--- src/test/java/com/cutanddry/qa/tests/catalog/VerifyPreviewCatalogItemDetailsCatalogDataTest.java
+++ src/test/java/com/cutanddry/qa/tests/catalog/VerifyPreviewCatalogItemDetailsCatalogDataTest.java
@@ -42,6 +42,9 @@ public class VerifyPreviewCatalogItemDetailsCatalogDataTest extends TestBase {
         Catalog.selectProductActiveInactiveStatus(InActive);
         Catalog.saveChanges();
         softAssert.assertTrue(Catalog.successOverlayDisplayed(),"Error in saving item data in catalog");
+        Catalog.selectProductActiveInactiveStatus(Active);
+        Catalog.saveChanges();
+        softAssert.assertTrue(Catalog.successOverlayDisplayed(),"Error in saving item data in catalog");
         softAssert.assertAll();
     }
 
