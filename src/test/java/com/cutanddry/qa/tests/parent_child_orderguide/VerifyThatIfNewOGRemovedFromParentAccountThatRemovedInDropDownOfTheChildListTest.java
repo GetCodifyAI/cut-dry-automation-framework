a02c228f9a9e4e18392143109693dcb70dd8bae3
virangabandara <viranga@cutanddry.com>
2025-05-08 08:27:49 +0530

    add VerifyThatIfNewOGRemovedFromParentAccountThatRemovedInDropDownOfTheChildListTest

---
+++ src/test/java/com/cutanddry/qa/tests/parent_child_orderguide/VerifyThatIfNewOGRemovedFromParentAccountThatRemovedInDropDownOfTheChildListTest.java
@@ -0,0 +1,100 @@
+package com.cutanddry.qa.tests.parent_child_orderguide;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.data.testdata.ParentChildOGData;
+import com.cutanddry.qa.functions.Customer;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Orders;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyThatIfNewOGRemovedFromParentAccountThatRemovedInDropDownOfTheChildListTest extends TestBase {
+    static User user;
+    static String DP = ParentChildOGData.DISTRIBUTOR_INDIANHEAD;
+    static String customerId = ParentChildOGData.CUSTOMER_ID_INDIANHEAD;
+    static String parentStatus = "N/A";
+    static String OrderGuideName = ParentChildOGData.ORDER_GUIDE_NAME_2;
+    static String itemName = "Appetizer Egg Roll Vegetable";
+    static String[] childAccounts = { "13410", "13403", "13400", "13414", "13412" };
+    static String childSettingMessage = "Child account settings updated successfully";
+    static String parentOrderGuide = ParentChildOGData.PARENT_ORDER_GUIDE_NAME;
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-1217")
+    public void VerifyThatIfNewOGRemovedFromParentAccountThatRemovedInDropDownOfTheChildList() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Assert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToDistributorPortal(DP);
+        Assert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"navigation error");
+
+        Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(customerId);
+        Assert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(customerId),"search error");
+        Customer.clickOnOrderGuide(customerId);
+        softAssert.assertTrue(Customer.isCustomerOrderGuideDisplayed(),"user has navigated to the Order Guide");
+        Customer.goToCreatePopup();
+        Customer.createOrderGuide(OrderGuideName);
+        Customer.createOrderFromCatalog();
+        Customer.searchItemOnCatalog(itemName);
+        Customer.addItemFromCatalog();
+        Customer.closeEditorCatalog();
+
+        Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(customerId);
+        Assert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(customerId),"search error");
+        Customer.SelectCustomer(customerId);
+        softAssert.assertTrue(Customer.isLinkedAccountDisplayed(),"linked account section not displayed");
+        softAssert.assertTrue(Customer.isParentAccountStatusDisplayed(parentStatus),"parent account status not displayed");
+        Customer.clickEditChildAccount();
+        softAssert.assertTrue(Customer.isManageChildAccountPopUpDisplayed(),"manage child account pop up not displayed");
+        for (String childAccount : childAccounts) {
+            Customer.selectNewlyAddedOrderGuide(childAccount,OrderGuideName);
+        }
+        Orders.clickSaveButton();
+        softAssert.assertTrue(Customer.isChildSettingUpdated(childSettingMessage),"child setting not updated");
+        Customer.clickOK();
+
+        Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(customerId);
+        Assert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(customerId),"search error");
+        Customer.clickOnOrderGuide(customerId);
+        softAssert.assertTrue(Customer.isCustomerOrderGuideDisplayed(),"user has navigated to the Order Guide");
+        Customer.clickOGDropdown();
+        Customer.selectNewlyCreatedOrderGuide(OrderGuideName);
+        Customer.goToEdit();
+        softAssert.assertTrue(Customer.isEditOrderGuideTextDisplayed(),"navigation error for edit");
+        Customer.expandMoreOptionsDropdown();
+        Customer.clickOnDeleteOrderGuide();
+        softAssert.assertTrue(Orders.isAreYouSurePopUpDisplayed(),"Are you sure pop up not displayed");
+        Orders.clickYes();
+
+        softAssert.assertTrue(Customer.isLinkedAccountDisplayed(),"linked account section not displayed");
+        softAssert.assertTrue(Customer.isParentAccountStatusDisplayed(parentStatus),"parent account status not displayed");
+        Customer.clickEditChildAccount();
+        softAssert.assertTrue(Customer.isManageChildAccountPopUpDisplayed(),"manage child account pop up not displayed");
+        for (String childAccount : childAccounts) {
+            softAssert.assertTrue(Customer.isChildAccountOGDisplayed(childAccount,parentOrderGuide),"not remove parent order guide");
+        }
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
