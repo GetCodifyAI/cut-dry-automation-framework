89de3ca3cbc21647ca3867900eb15578edd3528a
virangabandara <viranga@cutanddry.com>
2025-06-24 22:59:57 +0530

    add VerifyTheItemTypeFilterIsVisibleInPublicCatalogTest

---
+++ src/test/java/com/cutanddry/qa/tests/customer_catalog/VerifyTheItemTypeFilterIsVisibleInPublicCatalogTest.java
@@ -0,0 +1,51 @@
+package com.cutanddry.qa.tests.customer_catalog;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Catalog;
+import com.cutanddry.qa.functions.Customer;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyTheItemTypeFilterIsVisibleInPublicCatalogTest extends TestBase {
+    static User user;
+    static String itemType = "Item Type";
+    static String itemTypeOption = "Stocked (in-house)";
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-1480")
+    public void VerifyTheItemTypeFilterIsVisibleInPublicCatalog() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+
+        Dashboard.navigateToCatalog();
+        Assert.assertTrue(Catalog.isUserNavigatedToCatalog(),"navigation error");
+        Catalog.clickOnPreviewCatalog();
+        Assert.assertTrue(Catalog.isNavigatedToPreviewCatalog(),"navigation to preview catalog error");
+        softAssert.assertTrue(Customer.isCatalogFilterDisplayed(itemType),"catalog filter not display");
+        Customer.clickItemTypeFilter();
+        Customer.clickItemTypeFilterOption(itemTypeOption);
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result){
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
