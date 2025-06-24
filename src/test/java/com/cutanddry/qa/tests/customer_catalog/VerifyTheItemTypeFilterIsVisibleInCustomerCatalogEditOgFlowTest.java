a1c3c5c9e4e827b815e9e8e098abf06625625630
virangabandara <viranga@cutanddry.com>
2025-06-24 22:59:43 +0530

    add VerifyTheItemTypeFilterIsVisibleInCustomerCatalogEditOgFlowTest

---
+++ src/test/java/com/cutanddry/qa/tests/customer_catalog/VerifyTheItemTypeFilterIsVisibleInCustomerCatalogEditOgFlowTest.java
@@ -0,0 +1,57 @@
+package com.cutanddry.qa.tests.customer_catalog;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Catalog;
+import com.cutanddry.qa.functions.Customer;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyTheItemTypeFilterIsVisibleInCustomerCatalogEditOgFlowTest extends TestBase {
+    static User user;
+    String CustomerCode = "16579";
+    static String itemType = "Item Type";
+    static String itemTypeOption = "Stocked (in-house)";
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-1479")
+    public void VerifyTheItemTypeFilterIsVisibleInCustomerCatalogEditOgFlow() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToCustomers();
+        softAssert.assertTrue(Customer.isCustomersTextDisplayed(),"customer section not display");
+        Customer.searchCustomerByCode(CustomerCode);
+        softAssert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(CustomerCode),"customer not found");
+        Customer.clickOnOrderGuide(CustomerCode);
+
+        Customer.goToEdit();
+        softAssert.assertTrue(Customer.isEditOrderGuideTextDisplayed(),"navigation error for edit");
+        Customer.createOrderFromCatalog();
+        softAssert.assertTrue(Customer.isCatalogFilterDisplayed(itemType),"catalog filter not display");
+        Customer.clickItemTypeFilter();
+        Customer.clickItemTypeFilterOption(itemTypeOption);
+        Customer.closeEditorCatalog();
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result){
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
