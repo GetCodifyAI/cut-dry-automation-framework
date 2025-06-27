3853a8a768700364aa864314ab2f93b3337db1b6
virangabandara <viranga@cutanddry.com>
2025-06-27 09:04:43 +0530

    add VerifyDpsHaveSpecialItemsOrderSubmissionTest

---
+++ src/test/java/com/cutanddry/qa/tests/catalog/VerifyDpsHaveSpecialItemsOrderSubmissionTest.java
@@ -0,0 +1,87 @@
+package com.cutanddry.qa.tests.catalog;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.*;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyDpsHaveSpecialItemsOrderSubmissionTest extends TestBase {
+    SoftAssert softAssert;
+    static User user;
+    static String customerId = "16579";
+    String DistributorName ="47837013 - Brandon IFC Cut+Dry Agent - Independent Foods Co";
+    static String SpecialItemsDropdownOption1 = "Enabled (via Manual Selection)";
+    String itemCode = "00036";
+    static String specialItemYes = "Yes";
+    static String specialItemNo = "No";
+    String All = "All";
+
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+
+    @Test(groups = "DOT-TC-1426")
+    public void VerifyDpsHaveSpecialItemsOrderSubmission() throws InterruptedException {
+
+        softAssert = new SoftAssert();
+        Login.logIntoRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Assert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToInternalToolsPage();
+        InternalTools.navigateToConfigureSupplier();
+        InternalTools.navigateToIndependentCompEditDetails();
+        InternalTools.navigateToCatalogSettingsTab();
+        InternalTools.selectSpecialItemsDropdown(SpecialItemsDropdownOption1);
+        InternalTools.catalogSettingsSave();
+
+        Login.navigateToDistributorPortal(DistributorName);
+        Assert.assertTrue(Dashboard.isUserNavigatedToDashboard(), "The user is unable to land on the Dashboard page.");
+        Dashboard.navigateToCatalog();
+        softAssert.assertTrue(Catalog.isUserNavigatedToCatalog(),"navigation error");
+        Catalog.selectItemStatus(All);
+        Catalog.selectItemFromGrid(itemCode);
+        softAssert.assertEquals(Catalog.getItemcodeInCatalogData(),itemCode,"Error in getting Item Code");
+        softAssert.assertTrue(Catalog.isSpecialItemDropDownDisplay(),"special item drop down display error");
+        Catalog.clickOnSpecialItem(specialItemYes);
+        Catalog.saveChanges();
+        softAssert.assertTrue(Catalog.successOverlayDisplayed(),"Error in saving item data in catalog");
+
+        Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(customerId);
+        Assert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(customerId), "Unable to find the customer Id");
+        Customer.clickOnOrderGuide(customerId);
+        Customer.searchItemOnOrderGuide(itemCode);
+        Customer.increaseFirstRowQtyCustom(1);
+        Customer.checkoutItems();
+
+        softAssert.assertTrue(Customer.isReviewOrderTextDisplayed(), "The user is unable to land on the Review Order page.");
+        Customer.submitOrder();
+        softAssert.assertTrue(Customer.isThankingForOrderPopupDisplayed(), "The order was not completed successfully.");
+        Customer.clickClose();
+
+        Dashboard.navigateToCatalog();
+        softAssert.assertTrue(Catalog.isUserNavigatedToCatalog(),"navigation error");
+        Catalog.selectItemStatus(All);
+        Catalog.selectItemFromGrid(itemCode);
+        softAssert.assertEquals(Catalog.getItemcodeInCatalogData(),itemCode,"Error in getting Item Code");
+        softAssert.assertTrue(Catalog.isSpecialItemDropDownDisplay(),"special item drop down display error");
+        Catalog.clickOnSpecialItem(specialItemNo);
+        Catalog.saveChanges();
+        softAssert.assertTrue(Catalog.successOverlayDisplayed(),"Error in saving item data in catalog");
+        softAssert.assertAll();
+    }
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsersAtOnce();
+    }
+}
