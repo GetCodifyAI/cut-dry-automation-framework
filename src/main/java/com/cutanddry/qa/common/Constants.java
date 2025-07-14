c2ba9972675e3a82378282200e6c9cef68007659
AhsanMubarak <ahsan@cutanddry.com>
2025-07-14 12:40:00 +0530

    Introduce the sanity suite in DP

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -9,17 +9,17 @@ public class Constants {
     public static String TEST_ENV = System.getProperty("test.env", "uat");
     public static String MAIN_URL = baseDomain();
     public static String SEC_URL = secDomain();
-    public static String LOGIN_AS = "https://app-uat.staging.cutanddry.com/internaltools/loginas";
-    public static String NODE_EXPLORER = "https://app-uat.staging.cutanddry.com/admin";
+    public static String LOGIN_AS = loginAsDomain();
+    public static String NODE_EXPLORER = nodeExplorerDomain();
     public static String BASE_URI = "https://supplier-uat.staging.cutanddry.com/GraphQLController";
-    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
-    public static String CONFIG_SUPPLIER = "https://app-uat.staging.cutanddry.com/internaltools/configure-supplier";
+    public static String CONFIG_SUPPLIER = configSupplierDomain();
     public static String GATE_KEEPER_ADMIN = "https://app-uat.staging.cutanddry.com/gatekeeperadmin";
-
     public static String PROD_LOGIN_AS = "https://internal.cutanddry.com/internaltools/loginas";
 
-    //  https://hooks.slack.com/services/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 //group - test-alerts
-    //  https://hooks.slack.com/services/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 //group - ui-automation-tests
+    //Environment variables
+    //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
+    public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
+    public static final String TEST_STAG ="ddo-800";
 
 
     private static String baseDomain() {
@@ -27,6 +27,8 @@ public class Constants {
             MAIN_URL = "https://supplier-uat.staging.cutanddry.com/";
         } else if (TEST_ENV.equalsIgnoreCase("PROD")){
             MAIN_URL = "https://supplier.cutanddry.com/";
+        } else if (TEST_ENV.equalsIgnoreCase("STAG")) {
+            MAIN_URL = "https://supplier-"+TEST_STAG+".staging.cutanddry.com/";
         }
         return MAIN_URL;
     }
@@ -35,7 +37,42 @@ public class Constants {
             SEC_URL = "https://app-"+TEST_ENV+".staging.cutanddry.com/";
         } else if (TEST_ENV.equalsIgnoreCase("PROD")){
             SEC_URL = "https://app.cutanddry.com/";
+        } else if (TEST_ENV.equalsIgnoreCase("STAG")) {
+            SEC_URL = "https://app-"+TEST_STAG+".staging.cutanddry.com/";
         }
         return SEC_URL;
+    }
+
+    private static String loginAsDomain() {
+        if (TEST_ENV.equalsIgnoreCase("UAT")) {
+            LOGIN_AS = "https://app-uat.staging.cutanddry.com/internaltools/loginas";
+        } else if (TEST_ENV.equalsIgnoreCase("PROD")) {
+            LOGIN_AS = "https://app.cutanddry.com/internaltools/loginas";
+        } else if (TEST_ENV.equalsIgnoreCase("STAG")) {
+            LOGIN_AS = "https://app-"+TEST_STAG+".staging.cutanddry.com/internaltools/loginas";
+        }
+        return LOGIN_AS;
+    }
+
+    private static String nodeExplorerDomain() {
+        if (TEST_ENV.equalsIgnoreCase("UAT")) {
+            NODE_EXPLORER = "https://app-uat.staging.cutanddry.com/admin";
+        } else if (TEST_ENV.equalsIgnoreCase("PROD")) {
+            NODE_EXPLORER = "https://app.cutanddry.com/admin";
+        } else if (TEST_ENV.equalsIgnoreCase("STAG")) {
+            NODE_EXPLORER = "https://app-"+TEST_STAG+".staging.cutanddry.com/admin";
+        }
+        return NODE_EXPLORER;
+    }
+
+    private static String configSupplierDomain() {
+        if (TEST_ENV.equalsIgnoreCase("UAT")) {
+            CONFIG_SUPPLIER = "https://app-uat.staging.cutanddry.com/internaltools/configure-supplier";
+        } else if (TEST_ENV.equalsIgnoreCase("PROD")) {
+            CONFIG_SUPPLIER = "https://app.cutanddry.com/internaltools/configure-supplier";
+        } else if (TEST_ENV.equalsIgnoreCase("STAG")) {
+            CONFIG_SUPPLIER = "https://app-"+TEST_STAG+".staging.cutanddry.com/internaltools/configure-supplier";
+        }
+        return CONFIG_SUPPLIER;
     }
 }
