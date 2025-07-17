89cb065be98caa25059ab24786c29ccef50b17ee
UmeshIshan25 <umesh@cutanddry.com>
2025-07-17 11:24:35 +0530

    Proprietary items fix quick add flow

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="ddo-800";
+    public static final String TEST_STAG ="cat-prop";
 
 
     private static String baseDomain() {
