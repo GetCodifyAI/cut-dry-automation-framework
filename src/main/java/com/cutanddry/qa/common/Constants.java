1e3b57fea2c30a47eb40f2d5400d006b5bd3bbe8
wishminipalihawadana <wishmini@cutanddry.com>
2025-07-18 14:02:56 +0530

    testing Canonwise Default UOM

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="ishan-defunit";
+    public static final String TEST_STAG ="ish-defuom";
 
 
     private static String baseDomain() {
