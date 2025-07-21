cbb0c6d9a49c7b9cb89f6207b0bf9d857c7e0fd2
wishminipalihawadana <wishmini@cutanddry.com>
2025-07-21 15:59:58 +0530

    testing email subject for special order

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="cs-v3";
+    public static final String TEST_STAG ="psf-mahe";
 
 
     private static String baseDomain() {
