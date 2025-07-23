3e861df4abb86f8b083943eb8a711632ef9be7ec
MRathnayaka <mrathnayaka@mitrai.com>
2025-07-23 19:08:36 +0530

    Staging Test for or-cut

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="track-827-1";
+    public static final String TEST_STAG ="or-cut";
 
 
 
