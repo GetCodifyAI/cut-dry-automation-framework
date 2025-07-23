4c41e1c0c8916302eac40959fb5b52794cc94669
wishminipalihawadana <wishmini@cutanddry.com>
2025-07-23 08:26:25 +0530

    testing edit view

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,9 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="ddo-846";
-
-
+    public static final String TEST_STAG ="editbutton";
 
 
     private static String baseDomain() {
