d50c417171f76de6df190c98a6dbda1ebdb89cda
dileesha <dileesha@cutanddry.com>
2025-07-28 12:04:38 +0530

    draft-issues

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="ddo-857";
+    public static final String TEST_STAG ="draft-issues";
 
 
     private static String baseDomain() {
