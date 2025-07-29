4e0d0d6361fbd0884e51ae608a855935c6d3c6ae
wishminipalihawadana <wishmini@cutanddry.com>
2025-07-29 07:52:07 +0530

    testing Order History UI/UX fixes

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,8 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="dot-8955";
-
+    public static final String TEST_STAG ="ddo-851";
 
 
     private static String baseDomain() {
