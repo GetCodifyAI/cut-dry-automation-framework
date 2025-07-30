35746c0ab0a5661168d62a69456a9b7a1eeb603a
hasini-Kavindya <hasini@cutanddry.com>
2025-07-30 08:14:32 +0530

    Added dot-9604 DP side V2

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -22,7 +22,6 @@ public class Constants {
     public static final String TEST_STAG ="dot-9604";
 
 
-
     private static String baseDomain() {
         if (TEST_ENV.equalsIgnoreCase("UAT")){
             MAIN_URL = "https://supplier-uat.staging.cutanddry.com/";
