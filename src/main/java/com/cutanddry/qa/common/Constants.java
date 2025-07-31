244b586600de2d4b2813f3b6ebf878a508481b10
hasini-Kavindya <hasini@cutanddry.com>
2025-07-31 20:23:22 +0530

    Added ddo-892 DP side

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="payv2-712";
+    public static final String TEST_STAG ="ddo-892";
 
 
     private static String baseDomain() {
