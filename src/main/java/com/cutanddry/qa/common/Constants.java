d1d9a06c2a5f048f291e3fbf45b7497a43fad2c5
wishminipalihawadana <wishmini@cutanddry.com>
2025-08-01 15:12:15 +0530

    testing sentry bug:Stock level

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="cw-swt-rn";
+    public static final String TEST_STAG ="sentry-20";
 
 
     private static String baseDomain() {
