ae62b5a842272f1e8e02a0bc2d77444351cbb7bc
hasini-Kavindya <hasini@cutanddry.com>
2025-08-06 09:45:03 +0530

    Added ddo-858-2 DP side2

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -22,6 +22,7 @@ public class Constants {
     public static final String TEST_STAG ="ddo-858-2";
 
 
+
     private static String baseDomain() {
         if (TEST_ENV.equalsIgnoreCase("UAT")){
             MAIN_URL = "https://supplier-uat.staging.cutanddry.com/";
