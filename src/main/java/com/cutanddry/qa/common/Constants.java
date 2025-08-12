67cd59e521ec7f324eef913251ccfafe26500c39
UmeshIshan25 <umesh@cutanddry.com>
2025-08-12 13:32:45 +0530

    Sanity for standing order change

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="dot9733";
+    public static final String TEST_STAG ="standing-order";
 
 
     private static String baseDomain() {
