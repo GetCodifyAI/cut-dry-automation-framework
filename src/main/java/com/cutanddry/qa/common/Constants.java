79e126c08818442638cf30612add75c3a70feb59
dileesha <dileesha@cutanddry.com>
2025-08-13 09:16:55 +0530

    dot-9903

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="ddo855v5";
+    public static final String TEST_STAG ="dot-9903";
 
 
     private static String baseDomain() {
