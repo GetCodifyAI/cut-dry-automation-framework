bd9b20e473a0da9e9b6cf4ac40d11fb23f79eb28
MRathnayaka <mrathnayaka@mitrai.com>
2025-08-22 14:35:12 +0530

    Staging Test for uat

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="standing-order";
+    public static final String TEST_STAG ="dot-9989";
 
 
     private static String baseDomain() {
