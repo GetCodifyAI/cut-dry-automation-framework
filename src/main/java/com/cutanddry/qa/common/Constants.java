da7eba2a2c03d2eaffc465ca74dbf0822cfc687e
hasini-Kavindya <hasini@cutanddry.com>
2025-09-15 13:03:21 +0530

    Added ddo-911 run DP side

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     //Environment variables
 //    public static String SLACK_WEBHOOK = System.getenv("SLACK_WEBHOOK");
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
-    public static final String TEST_STAG ="psup-696";
+    public static final String TEST_STAG ="ddo-911";
 
 
     private static String baseDomain() {
