5f7f0a971773530fcc166cd0e325e7ad457c01c8
UmeshIshan25 <umesh@cutanddry.com>
2025-12-25 20:00:56 +0530

    Automating test cases

--- src/test/java/com/cutanddry/qa/tests/scan_to_order/VerifyScanItemsToOrderFunctionalityInMoreOptionsMenuTest.java
+++ src/test/java/com/cutanddry/qa/tests/scan_to_order/VerifyScanItemsToOrderFunctionalityInMoreOptionsMenuTest.java
@@ -18,7 +18,9 @@ public class VerifyScanItemsToOrderFunctionalityInMoreOptionsMenuTest extends Te
     static String featureName = "scan_to_order";
     static String companyID = "46017666";
     static String DP = "Independent Foods Co";
+    static String featureDisabledDp = "138629491 - Eshan - What Chefs Want - Rockies";
     static String CustomerCode = "21259";
+    static String featureDisabledCustomerCode = "110RES";
     static String ItemCode;
 
     @BeforeMethod
@@ -39,25 +41,34 @@ public class VerifyScanItemsToOrderFunctionalityInMoreOptionsMenuTest extends Te
 
         Login.navigateToDistributorPortal(DP);
         Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(CustomerCode);
+        softAssert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(CustomerCode), "Unable to find the customer Id");
         Customer.clickOnOrderGuide(CustomerCode);
         ItemCode = Customer.getItemCodeFirstRow();
-
         Customer.expandMoreOptionsDropdown();
         softAssert.assertTrue(Customer.isScanItemsToOrderDisplayedInMoreOptions(), "Scan Items to Order option is not displayed in More Options menu");
-
         Customer.clickOnScanItemsToOrder();
         softAssert.assertTrue(ScanToOrder.isNavigatedToScanToOrderPage(), "Error in navigating to Scan to Order screen");
 
-        softAssert.assertFalse(ScanToOrder.isReviewAndConfirmBtnEnabled(), "Review and Confirm Button should be disabled initially");
         softAssert.assertFalse(ScanToOrder.isAddToCartButtonEnabled(), "Add to Cart Button should be disabled initially");
+        ScanToOrder.scanOrEnterItemCode(ItemCode);
 
-        ScanToOrder.selectScanToOrderItemInputField();
-        ScanToOrder.enterItemCodeToScanToOrderItemInputField(ItemCode);
-        softAssert.assertTrue(ScanToOrder.isAddToCartButtonEnabled(), "Add to Cart Button is not enabled after entering item code");
-
+        ScanToOrder.AddItemsToOrder();
+        softAssert.assertTrue(Customer.isItemAdded(ItemCode), "Item is not added to the cart");
+        Customer.clickOnBackButton();
+        ScanToOrder.scanOrEnterItemCode(ItemCode);
+        ScanToOrder.ClearAll();
         ScanToOrder.AddItemsToCart();
-        softAssert.assertTrue(ScanToOrder.isItemAddedToTheCart(ItemCode), "Item is not added to the cart");
-        softAssert.assertTrue(ScanToOrder.isReviewAndConfirmBtnEnabled(), "Review and Confirm Button is not enabled after adding item");
+        Customer.submitOrder();
+        softAssert.assertTrue(Customer.isThankingForOrderPopupDisplayed(), "The order was not completed successfully.");
+
+        Login.navigateToDistributorPortal(featureDisabledDp);
+        Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(featureDisabledCustomerCode);
+        softAssert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(featureDisabledCustomerCode), "Unable to find the customer Id");
+        Customer.clickOnOrderGuide(featureDisabledCustomerCode);
+        Customer.expandMoreOptionsDropdown();
+        softAssert.assertFalse(Customer.isScanItemsToOrderDisplayedInMoreOptions(), "Scan Items to Order option is displayed in More Options menu");
 
         softAssert.assertAll();
     }
